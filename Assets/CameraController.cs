7ac6d46048005a687f30c2ab68eb0122eed3817d
허승빈 <qmfhfl02@gmail.com>
2024-09-26 22:19:39 +0900

    9th Commit
    해상도 대응

---
+++ Assets/CameraController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float screenRatio;
+
+    private float refRatio = 1920f / 1080f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenRatio = (float)Screen.width / (float)Screen.height;
+        if(screenRatio<refRatio)
+            Camera.main.orthographicSize = 1920f/screenRatio/20f;
+        else
+        {
+            Camera.main.orthographicSize = 1080f / 20f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        screenRatio = (float)Screen.width / (float)Screen.height;
+        if(screenRatio<refRatio)
+            Camera.main.orthographicSize = 1920f/screenRatio/20f;
+        else
+        {
+            Camera.main.orthographicSize = 1080f / 20f;
+        }
+    }
+}
